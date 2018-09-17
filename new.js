179654ba7ebe7554ef26bb3c2d02a9bf210ee268
Vladanak <vladmyr89@gmail.com>
2018-09-17 11:32:28 +0300

    start

---
+++ new.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+var dir_path = process.argv[2];
+
+
+function FileSystem(path,fd)
+{
+    if(fs.existsSync(path))
+    {
+        fs.writeFile(`${path}` + "summary.js", "", function (err)
+        {
+            if (err)
+            {
+                throw err;
+            }
+            write(fd);
+        });
+    }
+
+}
