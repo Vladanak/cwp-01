24ed82b21937d9738f48d21d346fd47c01617e74
Vladanak <vladmyr89@gmail.com>
2018-09-18 21:42:40 +0300

    NewJs

---
+++ First.js
@@ -0,0 +1,12 @@
+const name = process.argv[2];
+
+console.log(`Hi ${name}!`);
+
+
+console.log(`2: ${process.argv[2]}`);
+console.log(`3: ${process.argv[3]}`);
+console.log(`4: ${process.argv[4]}`);
+console.log(`5: ${process.argv[5]}`);
+console.log(`6: ${process.argv[6]}`);
+console.log(`7: ${process.argv[7]}`);
+console.log(`8: ${process.argv[8]}`);
