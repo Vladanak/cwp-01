e46c2c83ac3e83e927c364e554dd8dd1e00db7c4
Vladanak <vladmyr89@gmail.com>
2018-09-24 08:44:51 +0300

    liittle update

--- summary.js
+++ summary.js
@@ -1,7 +1,7 @@
-console.log('..\\cwp-01an.txt');
+console.log('..\\cwp-01new.js');
 console.log('..\\cwp-01config.json');
 console.log('..\\cwp-01index.js');
-console.log('..\\cwp-01manyArguments.js');
+console.log('..\\cwp-01First.js');
 console.log('..\\cwp-01README.md');
 console.log('..\\cwp-01summary.js');
 console.log('directory1a.txt');
